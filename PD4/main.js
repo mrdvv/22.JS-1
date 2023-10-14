66450ce28c8360cdb9caa707c29aa70f3b91fa90
mrdvv <vilius.grumovskis@gmail.com>
2023-10-14 20:12:46 +0300

    PD4 and PD5 Done

---
+++ PD4/main.js
@@ -0,0 +1,12 @@
+let petName;
+let petAge;
+petName = "Danis";
+petAge = 13;
+console.log(petName);
+console.log(petAge);
+petName = "Rainys";
+petAge = 10;
+console.log(petName);
+console.log(petAge);
+const buildingAddress = "Ozo g. 12";
+console.log(buildingAddress);
---
+++ PD5/main.js
@@ -0,0 +1,38 @@
+let a = 12;
+let b = BigInt ("123456789012345678901234567890");
+let c = "asdasda";
+let d = true;
+let e = undefined;
+let f = null;
+let g = {Vardas:"Jonas"};
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+
+let number = 12;
+let otherNumber = 45.89;
+let textOne = 'Beautiful JavaScript World';
+let textTwo = "Beautiful JavaScript World";
+let textThree = `Beautiful JavaScript World`;
+let isTrue = (true == false);
+let isEqual = (2 >= 0);
+let petName = undefined;
+let petAge = null;
+let human = {
+    humanName: undefined,
+    humanAge: null
+};
+console.log(typeof number);
+console.log (typeof otherNumber);
+console.log (typeof textOne);
+console.log (typeof textTwo);
+console.log (typeof textThree);
+console.log (typeof isTrue);
+console.log (typeof isEqual);
+console.log (typeof PetName);
+console.log (typeof petAge);
+console.log (typeof human);
