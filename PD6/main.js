017c5ec4a972d0b631cc171e7dbfb0e543eca89d
mrdvv <118986012+mrdvv@users.noreply.github.com>
2023-11-26 20:38:23 +0200

    Kazkas

---
+++ PD6/main.js
@@ -0,0 +1,16 @@
+let funnyNumber = 12345; // Convert to String
+let funnyString = '12345'; // Convert to Number
+let funnyTrue = '1'; // Convert to Boolean
+let funnyFalse = '0'; // Convert to Boolean
+let isTrue = false; // Convert to String
+let isFalse = true; // Convert to Number
+let funnyText = 'JavaScript is dynamic programming language.'; // Convert to Number
+let funnyNull = null; // Convert to String
+let funnyUndefined = undefined; // Convert to String
+let isTrue = true; // Convert to String
+let isFalse = false; // Convert to String
+let booleanString = 'true'; // Convert to Number
+let otherBooleanString = 'false'; // Convert to Boolean
+
+String(funnyNumber);
+typeof funnyNumber;
